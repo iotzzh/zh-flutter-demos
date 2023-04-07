6e9f638a90d33dbaa77600f79ce052c579b1cf47
张中华 <956159241@qq.com>
2023-04-07 18:00:15 +0800

    初始化代码

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,222 @@
 import 'package:flutter/material.dart';
+import 'package:zh_flutter_demos/views/basic/text_style.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  static const String _title = 'Flutter 示例集合';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      title: _title,
+      theme: ThemeData.dark(useMaterial3: true),
+      home: Scaffold(
+        appBar: AppBar(title: const Text(_title), centerTitle: true),
+        body: const MyStatefulWidget(),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      routes: {
+        'text_style': (context) => const ZHTextStyle(),
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+// stores ExpansionPanel state information
+class Item {
+  Item({
+    required this.headerValue,
+    required this.buttons,
+    this.isExpanded = false,
+  });
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  String headerValue;
+  bool isExpanded;
+  List<ItemButton> buttons;
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class ItemButton {
+  ItemButton({
+    required this.text,
+    required this.routeName,
+  });
+  String text;
+  String routeName;
+}
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+List<Item> generateItems() {
+  var items = [
+    Item(headerValue: '基础组件', buttons: [
+      ItemButton(
+        text: '文本',
+        routeName: 'text_style',
+      ),
+      ItemButton(
+        text: '按钮',
+        routeName: 'text_style',
+      )
+    ]),
+    Item(headerValue: '布局类组件', buttons: [
+      ItemButton(
+        text: '文本',
+        routeName: 'text_style',
+      )
+    ]),
+    Item(headerValue: '容器类组件', buttons: [
+      ItemButton(
+        text: '文本',
+        routeName: 'text_style',
+      )
+    ]),
+    Item(headerValue: '可滚动组件', buttons: [
+      ItemButton(
+        text: '文本',
+        routeName: 'text_style',
+      )
+    ]),
+    Item(headerValue: '功能型组件', buttons: [
+      ItemButton(
+        text: '文本',
+        routeName: 'text_style',
+      )
+    ]),
+    Item(headerValue: '事件处理与通知', buttons: [
+      ItemButton(
+        text: '文本',
+        routeName: 'text_style',
+      )
+    ]),
+    Item(headerValue: '动画', buttons: [
+      ItemButton(
+        text: '文本',
+        routeName: 'text_style',
+      )
+    ]),
+    Item(headerValue: '自定义组件', buttons: [
+      ItemButton(
+        text: '文本',
+        routeName: 'text_style',
+      )
+    ]),
+    Item(headerValue: '文件操作', buttons: [
+      ItemButton(
+        text: '文本',
+        routeName: 'text_style',
+      )
+    ]),
+    Item(headerValue: '网络请求', buttons: [
+      ItemButton(
+        text: '文本',
+        routeName: 'text_style',
+      )
+    ]),
+    Item(headerValue: 'flutter扩展', buttons: [
+      ItemButton(
+        text: '文本',
+        routeName: 'text_style',
+      )
+    ]),
+    Item(headerValue: '国际化', buttons: [
+      ItemButton(
+        text: '文本',
+        routeName: 'text_style',
+      )
+    ]),
+    Item(headerValue: '其他', buttons: [
+      ItemButton(
+        text: '文本',
+        routeName: 'text_style',
+      )
+    ]),
+  ];
+
+  return List<Item>.generate(items.length, (int index) {
+    return Item(
+      headerValue: items[index].headerValue,
+      isExpanded: index == 0,
+      buttons: items[index].buttons,
+    );
+  });
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({super.key});
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  final List<Item> _data = generateItems();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return SingleChildScrollView(
+      child: Container(
+        child: _buildPanel(),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget _buildPanel() {
+    return ExpansionPanelList(
+      expansionCallback: (int index, bool isExpanded) {
+        setState(() {
+          _data[index].isExpanded = !isExpanded;
+        });
+      },
+      children: _data.map<ExpansionPanel>((Item item) {
+        return ExpansionPanel(
+          headerBuilder: (BuildContext context, bool isExpanded) {
+            return ListTile(
+              title: Text(item.headerValue),
+            );
+          },
+          body: Flex(
+            direction: Axis.horizontal,
+            children: item.buttons.map<Widget>((ItemButton button) {
+              return TextButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, button.routeName);
+                  },
+                  child: Text(
+                    button.text,
+                    textAlign: TextAlign.center,
+                  ));
+            }).toList(),
+            // [
+            //   ElevatedButton(
+            //     child: const Text(
+            //       '文本',
+            //       textAlign: TextAlign.center,
+            //       textDirection: TextDirection.ltr,
+            //     ),
+            //     onPressed: () {
+            //       Navigator.pushNamed(
+            //         context,
+            //         'text_style',
+            //       );
+            //     },
+            //   ),
+            //   TextButton(
+            //     child: const Text(
+            //       '文本',
+            //       textAlign: TextAlign.center,
+            //     ),
+            //     onPressed: () {
+            //       Navigator.pushNamed(
+            //         context,
+            //         'text_style',
+            //       );
+            //     },
+            //   ),
+            // ]
+          ),
+          isExpanded: item.isExpanded,
+        );
+      }).toList(),
     );
   }
 }
