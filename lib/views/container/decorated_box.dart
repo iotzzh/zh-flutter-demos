c14261c6fc962070d404e3d605b6410703ca4568
张中华 <956159241@qq.com>
2023-04-13 16:39:52 +0800

    添加装饰容器

--- lib/views/container/decorated_box.dart
+++ lib/views/container/decorated_box.dart
@@ -19,7 +19,29 @@ class ZHDecoratedBoxState extends State<ZHDecoratedBox> {
         body: ListView(
           shrinkWrap: true,
           padding: const EdgeInsets.all(20.0),
-          children: <Widget>[],
+          children: <Widget>[
+            DecoratedBox(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        colors: [Colors.red, Colors.orange.shade700]), //背景渐变
+                    borderRadius: BorderRadius.circular(3.0), //3像素圆角
+                    boxShadow: [
+                      //阴影
+                      BoxShadow(
+                          color: Colors.black54,
+                          offset: Offset(2.0, 2.0),
+                          blurRadius: 4.0)
+                    ]),
+                child: Padding(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 80.0, vertical: 18.0),
+                    child: Center(
+                      child: Text(
+                        "Login",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    )))
+          ],
         ));
   }
 }
